1ed7e335cee0038008b2bc88744c767ac9cad9be
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-02-09 23:02:19 +0300

    feat: add getSongsService

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import List from '../List';
+import getSongsService from '../../services/getSongsService';
 
 
 export default class App extends Component {
-  state = {}
+  constructor(props){
+    super(props);
+    this.getSongsService = new getSongsService();
+    this.state = {
+      songList: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getSongsService.getChart()
+      .then((response) => {
+        this.setState({
+          songList: response
+        })
+      });
+  }
 
   render() {
+    const { songList } = this.state;
     return (
       <div className="container">
-        <List />
+        <List songList={songList}/>
       </div>
       
     );
---
+++ src/services/getSongsService.js
@@ -0,0 +1,28 @@
+export default class getSongsService{
+  data = [
+    {
+      id: 1,
+      singer: 'Slipknot',
+      song: 'Wait and bleed',
+      genre: 'nu-metal',
+      year: 1999},
+    {
+      id: 2,
+      singer: 'Korn',
+      song: 'Falling Away from Me',
+      genre: 'nu-metal',
+      year: 1999},
+  ];
+
+  getChart = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.95) {
+          reject(new Error('Something bad happened'));
+        } else {
+          resolve(this.data);
+        }
+      }, 700);
+    });
+  }
+} 
